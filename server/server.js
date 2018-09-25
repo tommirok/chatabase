7aa267954b2fc41273948f706b01a70bcdf8689d
Tommi Rokolampi <tommi.rokolampi@hiq.fi>
2018-09-25 16:04:39 +0300

    port configured

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var PORT = process.env.PORT || 3000;
 app.get('/', function (req, res) {
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(3000, function () {
+http.listen(PORT, function () {
   console.log('listening on *:3000');
 });
