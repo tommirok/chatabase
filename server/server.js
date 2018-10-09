35a3c836eb0570e2cceea12b39386c6ed41f8094
Tommi Rokolampi <tommi.rokolampi@hiq.fi>
2018-10-09 06:19:22 +0300

    heroku ready... maybe

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 
 const express = require("express")
+const path = require("path")
 const routes = require("./routes/index")
 const bodyParser = require("body-parser")
 const userRoute = require("./routes/user")
@@ -8,17 +9,17 @@ const messageRoutes = require("./routes/messages")
 let app = express()
 const PORT = process.env.PORT || 5000
 app.use(bodyParser.json())
-
 app.use(bodyParser.urlencoded({ extended: true }))
-
-app.use("/", routes)
-
+app.use(express.static(path.join(__dirname, "client/build")))
 app.use("/user", userRoute)
 
 app.use("/topics", topicsRoute)
 
 app.use("/messages", messageRoutes)
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname + "/client/build/index.html"))
+})
 app.listen(PORT, function () {
 	console.log(`Listening on port ${PORT}`)
 })
