e7c5e03ebb8941e98d869ed67b3a5de15197e9b6
Tommi Rokolampi <tommi.rokolampi@hiq.fi>
2018-10-09 07:44:24 +0300

    no joko

--- server/server.js
+++ server/server.js
@@ -10,15 +10,16 @@ let app = express()
 const PORT = process.env.PORT || 5000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(path.join(__dirname, "client/build")))
+app.use("public", express.static(path.join(__dirname, "../client/build")))
+app.use("public", express.static(path.join(__dirname, "../client/public")))
 app.use("/user", userRoute)
 
 app.use("/topics", topicsRoute)
 
 app.use("/messages", messageRoutes)
 
-app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/build/index.html"))
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "../client/build/index.html"))
 })
 app.listen(PORT, function () {
 	console.log(`Listening on port ${PORT}`)
