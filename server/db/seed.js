899e568f139ed2f26f174e465c90e90cc6eae535
Tommi Rokolampi <tommi.rokolampi@hiq.fi>
2018-10-11 20:16:05 +0300

    seed planted for db initializing, todo: integrate with postgre db heroku

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,36 +1,44 @@
-var Database = require("./connect")
-
-var User = Database.sequelize.define("Users", {
-	id: { type: Database.DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-	username: Database.DataTypes.STRING,
-	password: Database.DataTypes.STRING
-})
-
-var Topic = Database.sequelize.define("Topic", {
-	id: { type: Database.DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-	name: Database.DataTypes.STRING,
-	description: Database.DataTypes.TEXT
-})
+var Database = require("../db/connect")
 
 var Message = Database.sequelize.define("Message", {
 	id: { type: Database.DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
 	title: Database.DataTypes.STRING,
-	content: Database.DataTypes.TEXT
-})
+	content: Database.DataTypes.TEXT,
+	userName: Database.DataTypes.STRING,
+}, { tableName: "Messages" })
 
 var Reply = Database.sequelize.define("Reply", {
 	id: { type: Database.DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
 	content: Database.DataTypes.TEXT
-})
+}, { tableName: "Replies" })
+
+var Topic = Database.sequelize.define("Topic", {
+	id: { type: Database.DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+	name: Database.DataTypes.STRING,
+	description: Database.DataTypes.TEXT,
+
+},
+	{ tableName: "Topics" })
+
+var User = Database.sequelize.define("User", {
+	id: { type: Database.DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+	username: Database.DataTypes.STRING,
+	password: Database.DataTypes.STRING,
+}, {
+		tableName: "Users"
+	})
 
 Message.belongsTo(User)
-Message.belongsTo(Topic)
-Reply.belongsTo(Message)
-Reply.belongsTo(User)
-
-Message.hasMany(Reply)
-Topic.hasMany(Message)
 User.hasMany(Message)
+
+Message.belongsTo(Topic)
+Topic.hasMany(Message)
+
+
+Reply.belongsTo(Message)
+Message.hasMany(Reply)
+
+Reply.belongsTo(User)
 User.hasMany(Reply)
 
 Database.sequelize.sync()
